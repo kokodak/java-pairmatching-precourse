2f181f34aec6a4b8927c4578d9b92516aa99ece6
kokodak <kokodakadokok@gmail.com>
2022-12-15 18:13:32 +0900

    feat(inputView): 과정, 레벨, 미션 선택 메서드 구현

--- src/main/java/pairmatching/util/InputValidator.java
+++ src/main/java/pairmatching/util/InputValidator.java
@@ -1,12 +1,22 @@
 package pairmatching.util;
 
+import java.util.List;
+
 public class InputValidator {
 
-    private static final String SELECT_KEY_ERROR = "[ERROR] 잘못된 기능을 선택하셨습니다.";
+    private static final String INPUT_ERROR = "[ERROR] 잘못된 형식입니다.";
 
     public static String validateSelectKey(String input) throws IllegalArgumentException {
         if (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("Q")) {
-            throw new IllegalArgumentException(SELECT_KEY_ERROR);
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+        return input;
+    }
+
+    public static String validateCourseLevelMission(String input) throws IllegalArgumentException {
+        String[] split = input.split(", ");
+        if (split.length != 3) {
+            throw new IllegalArgumentException(INPUT_ERROR);
         }
         return input;
     }
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -17,4 +17,15 @@ public class InputView {
             return getSelectKey();
         }
     }
+
+    public static String getCourseLevelMission() {
+        try {
+            System.out.println("과정, 레벨, 미션을 선택하세요.");
+            System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+            return InputValidator.validateCourseLevelMission(Console.readLine());
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return getCourseLevelMission();
+        }
+    }
 }
