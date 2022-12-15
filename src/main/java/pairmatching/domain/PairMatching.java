d476d408a6c8886e1f08d29ff8eb0d577dbaab2f
kokodak <kokodakadokok@gmail.com>
2022-12-15 13:29:11 +0900

    feat(pairMatching): 페어를 만드는 기능 구현

--- src/main/java/pairmatching/domain/PairMatching.java
+++ src/main/java/pairmatching/domain/PairMatching.java
@@ -1,2 +1,11 @@
-package pairmatching.domain;public class PairMatching {
+package pairmatching.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class PairMatching {
+
+    public static List<String> makePair(List<String> crews) {
+        return Randoms.shuffle(crews);
+    }
 }
