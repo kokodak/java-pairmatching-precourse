35d9295fb70a955b3b950a8a521502537cc5f6bd
kokodak <kokodakadokok@gmail.com>
2022-12-15 13:28:07 +0900

    feat(inputView): 입력 메서드 구현

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,21 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String getSelectKey() {
+        System.out.println("기능을 선택하세요.");
+        System.out.println("1. 페어 매칭");
+        System.out.println("2. 페어 조회");
+        System.out.println("3. 페어 초기화");
+        System.out.println("Q. 종료");
+        return Console.readLine();
+    }
+
+    public static String getInformation() {
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
+        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+        return Console.readLine();
+    }
+}
