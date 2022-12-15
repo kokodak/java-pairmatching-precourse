6c3ac5d81c3046ef25236e28ef2fe9cde1193b63
kokodak <kokodakadokok@gmail.com>
2022-12-15 21:34:35 +0900

    feat(crew): 크루 클래스 생성

---
+++ src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+public class Crew {
+
+    private Course course;
+    private String name;
+
+    Crew(Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+}
